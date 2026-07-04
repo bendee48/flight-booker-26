859376bad1e095cd92ac87d9142bc7b85ca12dc7
bendee <benbanj@hotmail.com>
2026-07-04 22:00:05 +0100

    Handling search data
    - Handling search data from params in Flight controller
    - Adding model methods to handle search querying
    - Refactoring departure dates in view. Add model method to return unique dates
    - Display crude flight search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,14 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    @departure_times = Flight.order(:start).select(:id, :start)
+    @departure_dates = Flight.unique_flight_dates
+    search = params[:search]
+
+    # if the search form has been submitted with all values present
+    if search&.values&.all?(&:present?)
+      @available_flights = Flight.find_flights(departure_airport_id: search[:departure_airport_id],
+                                               arrival_airport_id: search[:arrival_airport_id],
+                                               departure_date: search[:departure_date])
+    end
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,7 +2,12 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: :arrival_airport_id
   belongs_to :departure_airport, class_name: "Airport", foreign_key: :departure_airport_id
 
-  def formatted_date
-    start.strftime("%d/%m/%y (%H:%M)")
+  def self.find_flights(departure_airport_id:, arrival_airport_id:, departure_date:)
+    where(departure_airport_id: departure_airport_id, arrival_airport_id: arrival_airport_id)
+    .where("DATE(start) = ?", departure_date)
+  end
+
+  def self.unique_flight_dates
+    select("DISTINCT DATE(start) as start").map { |d| [ d.start.strftime("%a, %d %b %Y"), d.start ] }
   end
 end
