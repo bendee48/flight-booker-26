1d061f47937d0b89974075cb5573eeac2c96dfbf
bendee <benbanj@hotmail.com>
2026-07-05 21:32:14 +0100

    Displaying Flight Info
    - Refactoring flight controller so as to have variables that can be used for displaying the flight search information during different states
    - Adding a flight partial for displaying flight info
    - Rendering the available flight collection on the index page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,13 +2,22 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @departure_dates = Flight.unique_flight_dates
-    search = params[:search]
+    @search_submitted = all_search_fields_present?
 
-    # if the search form has been submitted with all values present
-    if search&.values&.all?(&:present?)
-      @available_flights = Flight.find_flights(departure_airport_id: search[:departure_airport_id],
-                                               arrival_airport_id: search[:arrival_airport_id],
-                                               departure_date: search[:departure_date])
+    if @search_submitted
+      @available_flights = Flight.find_flights(departure_airport_id: search_params[:departure_airport_id],
+                                               arrival_airport_id: search_params[:arrival_airport_id],
+                                               departure_date: search_params[:departure_date])
     end
+  end
+
+  private
+
+  def search_params
+    params[:search]
+  end
+
+  def all_search_fields_present?
+    search_params&.values&.all?(&:present?)
   end
 end
